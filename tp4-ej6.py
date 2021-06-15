a2f7b2499f9417321318db04ca294e156f795bb2
Ramiro Salcedo <ramirosdamian@gmail.com>
2021-06-15 00:26:45 -0300

    ejercicios

--- tp4-ej6.py
+++ tp4-ej6.py
@@ -19,16 +19,17 @@ def lista(cantidad_numeros):
     return lst
     
 
-def minimo(argumentolista, cantidad_numeros):
+def minimo(argumentolista):
     
     
-    
+    a = 1
     
     
     contador = 99999999999
     
-    for i in range(cantidad_numeros):
-              
+    for i in range(len(argumentolista)):
+        
+        a = a + 1
         valor = argumentolista[i]   
         
         if valor < contador:
@@ -43,14 +44,14 @@ def minimo(argumentolista, cantidad_numeros):
     return vminimo
 
 
-def maximo(argumentolista, cantidad_numeros):
+def maximo(argumentolista):
     
     
     
     
     contador = -999999999999
     
-    for i in range(cantidad_numeros):
+    for i in range(len(argumentolista)):
         
         valor = argumentolista[i] 
         
@@ -71,9 +72,9 @@ def prueba():
     cantidad_numeros = int(input("ingrese cuantos valores quiere cargar en la lista: "))
     listita = lista(cantidad_numeros)
     
-    min_n = minimo(listita, cantidad_numeros)
+    min_n = minimo(listita)
     print(f"El minimo es {min_n}")
-    max_n = maximo(listita, cantidad_numeros)
+    max_n = maximo(listita)
     print(f"El maximo es {max_n}")
 
 
